34e1a54c3fbb3d431a4b231ff08c82d6b0f755c0
bineethkumar <bineeth7@gmail.com>
2022-06-27 12:19:46 +0530

    Tailwind Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+<link href="/dist/output.css" rel="stylesheet"></link>
 function App() {
-  return <div className="App"> </div>
+  return <div className="App"> 
+  <h1 className="text-5xl">Hello</h1>
+  </div>
 }
 
 export default App;
